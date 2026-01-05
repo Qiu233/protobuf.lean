0494dba8b5fa3ec3a6da70b14a6189ffdacbfa87
Qiu233 <lopi258@outlook.com>
2026-01-06 00:14:57 +0800

    fix CI

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,7 @@ package "protobuf" where
   version := v!"0.1.0"
   leanOptions := #[ ⟨`experimental.module, true⟩ ]
 
-lean_lib «Protobuf» where
-
 @[default_target]
-lean_exe "protobuf" where
-  root := `Main
+lean_lib «Protobuf» where
 
 require binary from git "https://github.com/Qiu233/binary" @ "34c54ee3dda26c4bfaff0041464756df2ecc0760"
